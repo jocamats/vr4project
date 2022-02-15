e93b263bc7347d306e2130ccd780dfdc9354ec73
Jose Ignacio Camats <jose.camats@vr4it.com>
2022-02-15 17:22:11 +0000

    [ADD]:All on one

---
+++ odoo_academy/__manifest__.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Odoo Academy',
+    
+    'summary': """Academy app to manage Training""",
+    
+    'description': """
+        Academy Module to manage Training:
+        - Courses
+        - Sessions
+        - Attendees
+    """,
+    
+    'author': 'José Ignacio Camats',
+    
+    'website': 'https://www.odoo.com',
+    
+    'license':'LGPL-3',
+    
+    'category': 'Training',
+    'version': '0.1',
+    
+    'depends': ['base'],
+    
+    'data':[
+        'security/academy_security.xml',
+        'security/ir.model.access.csv',
+        'views/academy_menuitems.xml',
+        'views/course_views.xml',
+    ],
+    
+    'demo': [
+        'demo/academy_demo.xml',
+    ],
+    
+    
+}
